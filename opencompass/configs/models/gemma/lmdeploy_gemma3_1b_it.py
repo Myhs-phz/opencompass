2f5e47d907fd6e5c4de7721f4075f98e2a800134
Myhs-phz <demarcia2014@126.com>
2025-03-19 08:18:22 +0000

    feat gemma3

--- opencompass/configs/models/gemma/lmdeploy_gemma3_1b_it.py
+++ opencompass/configs/models/gemma/lmdeploy_gemma3_1b_it.py
@@ -9,6 +9,7 @@ models = [
         gen_config=dict(
             top_k=1, temperature=1e-6, top_p=0.9, max_new_tokens=8192
         ),
+        stop_words=['<end_of_turn>'],
         max_seq_len=32768,
         max_out_len=8192,
         batch_size=16,
